6289c46da9dde1c431c13d4b278c77b5fbd92637
Ulrich Wisser <ulrich@wisser.se>
2024-03-22 18:50:52 +0100

    Fix syntax error

--- node_helper.js
+++ node_helper.js
@@ -93,7 +93,7 @@ module.exports = NodeHelper.create({
             headers: {
                 'Content-Type: application/json',
                 'accept: application/json'
-            }
+            },
             json: true
         };
 
